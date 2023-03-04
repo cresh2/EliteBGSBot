d3041af3c9822865e3fea76553d32468f604348d
Calen Resh <cresh2@illinois.edu>
2023-03-04 13:57:36 -0600

    Removed express server

--- bot.js
+++ bot.js
@@ -39,15 +39,3 @@ for (const file of eventFiles) {
 }
 
 bot.login(process.env.TOKEN);
-
-const express = require('express')
-const app = express()
-const port = 10000
-
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
-
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
