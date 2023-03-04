3fce5c35e77b5cb7eba649e041084e604c741889
Calen Resh <cresh2@illinois.edu>
2023-03-04 11:58:38 -0600

    Added express to dependencies

--- bot.js
+++ bot.js
@@ -42,7 +42,7 @@ bot.login(process.env.TOKEN);
 
 const express = require('express')
 const app = express()
-const port = 3000
+const port = 10000
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
