26fdd7fab47c0b47eb12400aa82a929f819e3a5a
Calen Resh <cresh2@illinois.edu>
2023-03-06 09:15:34 -0600

    Added response

--- bot.js
+++ bot.js
@@ -47,10 +47,11 @@ const app = express();
 const port = 10000;
 
 app.get('/', (req, res) => {
+    res.send('Hello World!')
 });
 
 app.listen(port, () => {
-  //console.log(`Example app listening on port ${port}`)
+  console.log(`Example app listening on port ${port}`)
 });
 
 function preventTimeout() {
