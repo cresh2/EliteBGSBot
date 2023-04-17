3f4c81b5594f58e1073fd0ba4cd9dd8e34ddda10
Calen Resh <cresh2@illinois.edu>
2023-04-17 11:32:27 -0500

    Quick hotfix to keep the bot from reacting to every message

--- events/messageListener.js
+++ events/messageListener.js
@@ -23,7 +23,7 @@ module.exports = {
                 message.react('\u{274C}');
                 message.channel.send("Failed to parse log, make sure you are using the most up-to-date version of Hekateh's BGS tool.");
             }
-        } else {
+        } else if (message.content.includes('Date:') || message.content.includes('Target:')) {
             global.erroredLogs.push([message.id, "Unreadable log"]);
             message.react('\u{274C}');
             message.channel.send("Failed to parse log, make sure you are using the most up-to-date version of Hekateh's BGS tool.");
